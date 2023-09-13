68a613d7404c2024957d0039f3d24e1e290906c2
kemerald25 <126842942+kemerald25@users.noreply.github.com>
2023-09-13 07:54:43 +0100

    Update product.js

--- product.js
+++ product.js
@@ -1,3 +1,25 @@
+const  hambuger = document.querySelector(".hambuger"); 
+ const navMenu = document.querySelector(".nav-menu"); 
+  
+  
+ hambuger.addEventListener("click", () => { 
+     hambuger.classList.toggle("active"); 
+     navMenu.classList.toggle("active"); 
+ }) 
+  
+  
+  
+ document.querySelectorAll(".nav-links").forEach(n => n. 
+ addEventListener("click", () => { 
+     hambuger.classList.remove("active"); 
+     navMenu.classList.remove("active"); 
+ }));
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const showButton = document.getElementById("showButton");
     const blockPage = document.getElementById("blockPage");
